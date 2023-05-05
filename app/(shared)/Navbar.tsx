32d33d100cf182f3dba53215ad1b0fd5ff01eebb
Franco Traverso <franco-t20@outlook.com>
2023-05-05 20:55:46 -0300

    Add navbar links & layout

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -6,7 +7,15 @@ const Navbar = (props: Props) => {
   return (
     <header className='mb-5'>
       <nav className='flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4'>
-        nav
+        <div className='hidden sm:block'>Social Links</div>
+        <div className='flex justify-between items-center gap-10'>
+          <Link href='/'>Home</Link>
+          <Link href='/'>Trending</Link>
+          <Link href='/'>About</Link>
+        </div>
+        <div>
+          <p>Sign In</p>
+        </div>
       </nav>
     </header>
   );
