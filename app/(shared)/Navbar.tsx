ea6140200f042e97a5823062081826679973a429
Franco Traverso <franco-t20@outlook.com>
2023-05-04 23:35:25 -0300

    Add navbar component

---
+++ app/(shared)/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return (
+    <header className='mb-5'>
+      <nav className='flex justify-between items-center w-full bg-wh-900 text-wh-10 px-10 py-4'>
+        nav
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
+import Navbar from "app/(shared)/Navbar";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
