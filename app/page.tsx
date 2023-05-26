a8ccf5323995da372737e3e22f129b606010c525
Franco Traverso <franco-t20@outlook.com>
2023-05-26 19:27:31 -0300

    Set main page layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className='px-10 leading-7'>
+      <Trending />
+      <div className='md:flex gap-10 mb-5'>
+        <div className='basis-3/4'>
+          {/* <Tech /> */}
+          {/* <Travel /> */}
+          {/* <Other /> */}
+          <div className='hidden md:block'>{/* <Subscribe /> */}</div>
+        </div>
+        <div className='basis-1/4'>{/* <Sidebar/> */}</div>
+      </div>
+    </main>
+  );
 }
