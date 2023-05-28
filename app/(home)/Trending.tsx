66d702d3f9fcbb253900cbb9d9f165549f4f81a7
Franco Traverso <franco-t20@outlook.com>
2023-05-27 23:00:12 -0300

    Add trending element title & subtitle

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -13,6 +13,13 @@ const TrendingCard = ({ className }: TrendingCardProps) => {
       href='/'
     >
       <div className='z-0 relative w-full h-full bg-wh-500'>Image goes here</div>
+      <div className='absolute z-1 top-0 left-0 w-full h-full bg-gradient-gradual' />
+      <div className='absolute z-2 bottom-0 left-0 p-3'>
+        <h4 className='inline-block px-5 py-1 font-semibold bg-accent-orange text-wh-900'>
+          Category
+        </h4>
+        <div className='text-wh-100 mt-2'>post title</div>
+      </div>
     </Link>
   );
 };
