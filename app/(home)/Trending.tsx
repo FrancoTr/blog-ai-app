7e39c8b5a8422569265856b3646cf09a8193cc95
Franco Traverso <franco-t20@outlook.com>
2023-05-27 22:49:52 -0300

    Add trending grid content

--- app/(home)/Trending.tsx
+++ app/(home)/Trending.tsx
@@ -1,4 +1,21 @@
+import Link from "next/link";
 import React from "react";
+
+type TrendingCardProps = {
+  className?: string;
+};
+
+const TrendingCard = ({ className }: TrendingCardProps) => {
+  return (
+    <Link
+      className={`${className} sm:mt-0 sm:h-auto relative mt-7 block w-full h-96 hover:opacity-70`}
+      //href={`${process.env.NEXT_PUBLIC_URL}/POST/${post?.id}`}
+      href='/'
+    >
+      <div className='z-0 relative w-full h-full bg-wh-500'>Image goes here</div>
+    </Link>
+  );
+};
 
 type Props = {};
 
@@ -11,6 +28,12 @@ const Trending = (props: Props) => {
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur ad deserunt soluta
           id.
         </p>
+      </div>
+      <div className='sm:grid gap-5 grid-cols-4 grid-rows-2 sm:h-[600px] my-3'>
+        <div className='col-span-2 row-span-2 bg-wh-500'></div>
+        <div className='col-span-2 row-span-1 bg-wh-500'></div>
+        <div className='col-span-1 row-span-1 bg-wh-500'></div>
+        <div className='col-span-1 row-span-1 bg-wh-500'></div>
       </div>
     </section>
   );
