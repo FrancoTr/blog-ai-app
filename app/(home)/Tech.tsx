9c2c259a231547179cd9ad5ea15e47f2f26174ea
Franco Traverso <franco-t20@outlook.com>
2023-05-28 15:22:31 -0300

    Add tech section layout

--- app/(home)/Tech.tsx
+++ app/(home)/Tech.tsx
@@ -11,6 +11,12 @@ const Tech = (props: Props) => {
         <h4 className='bg-accent-orange py-2 px-5 text-wh-900 text-sm font-bold'>HOT</h4>
         <p className='font-bold text-2xl'>Latest News in technology</p>
       </div>
+      <div className='sm:grid grid-cols-2 grid-rows-3 gap-x-8 gap-y-8 my-5'>
+        <div className='bg-wh-500 col-span-1 row-span-3'></div>
+        <div className='bg-wh-500 col-span-1 row-span-1'></div>
+        <div className='bg-wh-500 col-span-1 row-span-1'></div>
+        <div className='bg-wh-500 col-span-1 row-span-1'></div>
+      </div>
     </section>
   );
 };
