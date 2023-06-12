c67f8670d6d8122318a54827a4741c93148cb094
Franco Traverso <franco-t20@outlook.com>
2023-06-11 23:30:50 -0300

    Add placeholder cards in other sections

--- app/(shared)/Other.tsx
+++ app/(shared)/Other.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Card from "./Card";
 
 type Props = {};
 
 const Other = (props: Props) => {
-  return <div>Other</div>;
+  return (
+    <section>
+      <hr className='border-1' />
+      {/* HEADER */}
+      <p className='font-bold text-2xl my-8'>Other Trending Posts</p>
+      <div className='sm:grid grid-cols-2 gap-16'>
+        <Card className='bg-wh-500 mt-5 sm:mt-0' imageHeight='h-80' />
+        <Card className='bg-wh-500 mt-5 sm:mt-0' imageHeight='h-80' />
+        <Card className='bg-wh-500 mt-5 sm:mt-0' imageHeight='h-80' />
+        <Card className='bg-wh-500 mt-5 sm:mt-0' imageHeight='h-80' />
+      </div>
+    </section>
+  );
 };
 
 export default Other;
